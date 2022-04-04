44219146c4cdaf6d66976aa26bad2e84a14cf64f
augustojs1 <augustojsouza1@gmail.com>
2022-04-04 11:07:05 -0300

    feat: setup passport to handle auth logic

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   controllers: [AuthenticationController],
   providers: [AuthenticationService],
-  imports: [UsersModule],
+  imports: [UsersModule, PassportModule],
 })
 export class AuthenticationModule {}
---
+++ src/authentication/local.strategy.ts
@@ -0,0 +1,19 @@
+import { Strategy } from 'passport-local';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authenticationService: AuthenticationService) {
+    super({
+      usernameField: 'email',
+    });
+  }
+  async validate(email: string, password: string) {
+    return await this.authenticationService.login({
+      email: email,
+      password: password,
+    });
+  }
+}
