7a3b68237eae3ebe9a24e5fd60d2cfab3640259b
augustojs1 <augustojsouza1@gmail.com>
2022-04-04 16:10:01 -0300

    docs: create documentantion for authentication routes

--- src/authentication/dto/login.dto.ts
+++ src/authentication/dto/login.dto.ts
@@ -1,11 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'email' })
   email: string;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ type: String, description: 'password' })
   password: string;
 }
