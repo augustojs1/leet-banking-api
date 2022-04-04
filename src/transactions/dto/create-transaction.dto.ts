7c63f3d1e25cb3160ec5d41e0ebc79731322a4a0
augustojs1 <augustojsouza1@gmail.com>
2022-04-04 13:22:37 -0300

    feat: create get and create transactions feature

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,1 +1,17 @@
-export class CreateTransactionDto {}
+import { Double } from 'bson';
+import { IsNumber, IsString, IsNotEmpty, IsPositive } from 'class-validator';
+
+export class CreateTransactionDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @IsPositive()
+  price: Double | number;
+
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+}
