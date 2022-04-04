80c5d3009ad1a7b092cf8c6f98becefd211e7890
augustojs1 <augustojsouza1@gmail.com>
2022-04-04 09:34:10 -0300

    feat: setting up mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
