51f8f21e6360f49f30f6ca32d68e8fb5dc621ded
augustojs1 <augustojsouza1@gmail.com>
2022-04-04 21:09:16 -0300

    docs: create transactions routes swagger documentation

--- src/transactions/dto/create-transaction.dto.ts
+++ src/transactions/dto/create-transaction.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNumber,
   IsString,
@@ -10,19 +11,23 @@ import { TransactionTypeEnum } from '../enum/transaction-type.enum';
 export class CreateTransactionDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   title: string;
 
   @IsNumber()
   @IsNotEmpty()
   @IsPositive()
+  @ApiProperty()
   ammount: number;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty()
   category: string;
 
   @IsString()
   @IsNotEmpty()
   @IsEnum(TransactionTypeEnum)
+  @ApiProperty()
   type: TransactionTypeEnum;
 }
